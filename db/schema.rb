95486f5a6fc9fa25969e55af4ee0128a9913861c
Robin Zuschke <robin.l.zuschke@gmail.com>
2018-02-09 14:26:39 +0100

    Migrated references to three tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180209124918) do
+ActiveRecord::Schema.define(version: 20180209132251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "shopping_cart_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+    t.index ["shopping_cart_id"], name: "index_cart_items_on_shopping_cart_id"
+  end
 
   create_table "product_categories", force: :cascade do |t|
     t.string "name"
@@ -30,12 +38,16 @@ ActiveRecord::Schema.define(version: 20180209124918) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "product_category_id"
+    t.index ["product_category_id"], name: "index_products_on_product_category_id"
   end
 
   create_table "shopping_carts", force: :cascade do |t|
     t.integer "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_shopping_carts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,4 +70,8 @@ ActiveRecord::Schema.define(version: 20180209124918) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cart_items", "products"
+  add_foreign_key "cart_items", "shopping_carts"
+  add_foreign_key "products", "product_categories"
+  add_foreign_key "shopping_carts", "users"
 end
